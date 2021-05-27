57874eb35692996a7b57b51f771720b3ba85b9a0
andikk <andikk@mail.ru>
2021-05-27 15:33:20 +0300

    Хоть что-то, хоть куда-то двигается )))

--- source/js/modules/calf.js
+++ source/js/modules/calf.js
@@ -1,4 +1,42 @@
 export default function animateCalf() {
+  let ctx = document.getElementById(`canvasCalf`).getContext(`2d`);
+  let calfImgDom = new Image();
 
+  const drawCalf = () => {
+    // рисуем изображение на постере
+    ctx.drawImage(calfImgDom, 0, 0, 300, 300);
+  };
+
+  // когда изображение загружено, рисуем его на холсте
+  calfImgDom.onload = () => {
+    drawCalf();
+  };
+
+
+  // задаём ссылку для изображения постера
+  calfImgDom.src = `/img/module-4/win-primary-images/sea-calf-2.png`;
+
+  let x = 1;
+  const animateCalfImg = () => {
+    x = x + 1;
+    ctx.drawImage(calfImgDom, x, 0, 300, 300);
+  };
+
+  const tick = () => {
+
+
+    // очищаем холст
+    ctx.clearRect(0, 0, 1500, 1500);
+    // сохраняем состояние контекста
+    ctx.save();
+    // рисуем сцену
+    animateCalfImg();
+    // восстанавливаем состояние контекста
+    ctx.restore();
+    // запускаем на отрисовку следующий кадр
+    window.requestAnimationFrame(tick);
+  };
+
+  window.requestAnimationFrame(tick);
 
 }
