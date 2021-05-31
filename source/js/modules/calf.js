9b971d1427193b41a40e856505083d4057f54752
andikk <andikk@mail.ru>
2021-05-31 10:39:13 +0300

    морж на льдине двигается

--- source/js/modules/calf.js
+++ source/js/modules/calf.js
@@ -1,42 +1,73 @@
 export default function animateCalf() {
-  let ctx = document.getElementById(`canvasCalf`).getContext(`2d`);
-  let calfImgDom = new Image();
+  const ctx = document.getElementById(`canvasCalf`).getContext(`2d`);
 
+  const calfImgDom = new Image();
   const drawCalf = () => {
-    // рисуем изображение на холсте
-    ctx.drawImage(calfImgDom, 0, 0, 300, 300);
+    ctx.drawImage(calfImgDom, 100, 300, 600, 600);
   };
-
-  // когда изображение загружено, рисуем его на холсте
   calfImgDom.onload = () => {
     drawCalf();
   };
-
-
-  // задаём ссылку для изображения
   calfImgDom.src = `/img/module-4/win-primary-images/sea-calf-2.png`;
 
-  let x = 1;
-  const animateCalfImg = () => {
-    x = x + 1;
-    ctx.drawImage(calfImgDom, x, 0, 300, 300);
+
+  const iceImgDom = new Image();
+  const drawIce = () => {
+    ctx.drawImage(iceImgDom, 150, 550, 500, 250);
+  };
+  iceImgDom.onload = () => {
+    drawIce();
+  };
+  iceImgDom.src = `/img/module-4/win-primary-images/ice.png`;
+
+
+
+  let y = 1;
+  const animateCalfImg = (progress) => {
+    y = progress * 100;
+    ctx.drawImage(iceImgDom, 150, 750 - y, 500, 250);
+    ctx.drawImage(calfImgDom, 100, 500 - y, 600, 600);
+
   };
 
-  const tick = () => {
+  function animate({timing, draw, duration}) {
 
+    let start = performance.now();
 
-    // очищаем холст
-    ctx.clearRect(0, 0, 1500, 1500);
-    // сохраняем состояние контекста
-    ctx.save();
-    // рисуем сцену
-    animateCalfImg();
-    // восстанавливаем состояние контекста
-    ctx.restore();
-    // запускаем на отрисовку следующий кадр
-    window.requestAnimationFrame(tick);
-  };
+    // eslint-disable-next-line no-shadow
+    requestAnimationFrame(function animate(time) {
+      // timeFraction изменяется от 0 до 1
+      let timeFraction = (time - start) / duration;
 
-  window.requestAnimationFrame(tick);
+      if (timeFraction > 1) {
+        timeFraction = 1;
+      }
+
+      // вычисление текущего состояния анимации
+      let progress = timing(timeFraction);
+      ctx.clearRect(0, 0, 800, 800);
+      // сохраняем состояние контекста
+      ctx.save();
+      // рисуем сцену
+      draw(progress); // отрисовать её
+      // восстанавливаем состояние контекста
+      ctx.restore();
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+
+    });
+  }
+
+  animate({
+    duration: 5000,
+    timing(timeFraction) {
+      return timeFraction * 4;
+    },
+    draw(progress) {
+      animateCalfImg(progress);
+    }
+  });
 
 }
