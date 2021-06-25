b6d3905b214b804e30fb04f01c069e9e7c2693a9
andikk <andikk@mail.ru>
2021-06-25 12:47:40 +0300

    появление

--- source/js/modules/calf.js
+++ source/js/modules/calf.js
@@ -1,19 +1,16 @@
 export default function animateCalf() {
   const ctx = document.getElementById(`canvasCalf`).getContext(`2d`);
 
-  let timeFraction = 1;
   const tick = () => {
-    timeFraction = timeFraction + 1;
     // очищаем холст
     ctx.clearRect(0, 0, 800, 800);
     // сохраняем состояние контекста
     ctx.save();
     // рисуем сцену
-
-
     drawObject({el: iceImgDom, dx: ice.dx, dy: ice.dy, dw: ice.dw, dh: ice.dh, isVisible: ice.isVisible});
     drawObject({el: calfImgDom, dx: calf.dx, dy: calf.dy, dw: calf.dw, dh: calf.dh, isVisible: calf.isVisible});
-    drawObject({el: snow1ImgDom, dx: snow1.dx, dy: snow1.dy, dw: snow1.dw, dh: snow1.dh, isVisible: snow1.isVisible});
+    drawObject({el: snowImgDom1, dx: snow1.dx, dy: snow1.dy, dw: snow1.dw, dh: snow1.dh, isVisible: snow1.isVisible});
+    drawObject({el: snowImgDom2, dx: snow2.dx, dy: snow2.dy, dw: snow2.dw, dh: snow2.dh, isVisible: snow2.isVisible});
     // восстанавливаем состояние контекста
     ctx.restore();
     // запускаем на отрисовку следующий кадр
@@ -35,18 +32,18 @@ export default function animateCalf() {
     return elImgDom;
   };
 
-  const animateIt = ({elFunction, started, duration, delay}) => {
+  const animateIt = ({elFunction, started, duration}) => {
     elFunction();
     let timePassed = Date.now() - started;
     if (timePassed < duration) {
-      requestAnimationFrame(() => animateIt({elFunction, started, duration, delay}));
+      requestAnimationFrame(() => animateIt({elFunction, started, duration}));
     }
   };
 
   // морж
   const calf = {
     dx: 130,
-    dy: 350,
+    dy: 550,
     dw: 550,
     dh: 550,
     src: `/img/module-4/win-primary-images/sea-calf-2.png`,
@@ -56,16 +53,13 @@ export default function animateCalf() {
   const calfImgDom = createImgElement(calf);
   const animateCalfImg = () => {
     calf.dy = (calf.dy - 10);
-    drawObject({el: calfImgDom, dx: calf.dx, dy: calf.dy, dw: calf.dw, dh: calf.dh, isVisible: 1});
-    if (calf.dy > 150) {
-      requestAnimationFrame(animateCalfImg);
-    }
+    drawObject({el: calfImgDom, dx: calf.dx, dy: calf.dy, dw: calf.dw, dh: calf.dh, isVisible: calf.isVisible});
   };
 
   // льдина
   const ice = {
     dx: 190,
-    dy: 600,
+    dy: 800,
     dw: 450,
     dh: 200,
     src: `/img/module-4/win-primary-images/ice.png`,
@@ -73,45 +67,57 @@ export default function animateCalf() {
   };
 
   const iceImgDom = createImgElement(ice);
-  const iceAnimation = () => {
+  const animateIceImg = () => {
     ice.dy = (ice.dy - 10);
     drawObject({el: iceImgDom, dx: ice.dx, dy: ice.dy, dw: ice.dw, dh: ice.dh, isVisible: ice.isVisible});
   };
 
-  const iceAnimation2 = () => {
-    ice.dy = (ice.dy + 10);
-    drawObject({el: iceImgDom, dx: ice.dx, dy: ice.dy, dw: ice.dw, dh: ice.dh, isVisible: ice.isVisible});
-  };
 
-
-  // снег
+  // снег 1
   const snow1 = {
     dx: 10,
-    dy: 600,
+    dy: 250,
     dw: 250,
     dh: 250,
     src: `/img/module-4/win-primary-images/snowflake.png`,
     isVisible: 0
   };
 
-  const snow1ImgDom = createImgElement(snow1);
-  const animateSnow1Img = () => {
+  const snowImgDom1 = createImgElement(snow1);
+  const animateSnowImg1 = () => {
     snow1.isVisible = 1;
-    snow1.dy = (snow1.dy - 10);
-    drawObject({el: snow1ImgDom, dx: snow1.dx, dy: snow1.dy, dw: snow1.dw, dh: snow1.dh, isVisible: snow1.isVisible});
-    if (snow1.dy > 400) {
-      requestAnimationFrame(animateSnow1Img);
-    }
+    snow1.dy = (snow1.dy - 1);
+    drawObject({el: snowImgDom1, dx: snow1.dx, dy: snow1.dy, dw: snow1.dw, dh: snow1.dh, isVisible: snow1.isVisible});
+  };
+
+
+  // снег 2
+  const snow2 = {
+    dx: 550,
+    dy: 250,
+    dw: 250,
+    dh: 250,
+    src: `/img/module-4/win-primary-images/snowflake.png`,
+    isVisible: 0
+  };
+
+  const snowImgDom2 = createImgElement(snow2);
+  const animateSnowImg2 = () => {
+    snow2.isVisible = 1;
+    snow2.dy = (snow2.dy - 1);
+    drawObject({el: snowImgDom2, dx: snow2.dx, dy: snow2.dy, dw: snow2.dw, dh: snow2.dh, isVisible: snow2.isVisible});
   };
 
 
   tick();
+  animateIt({elFunction: animateIceImg, started: Date.now(), duration: 600});
 
-  animateIt({elFunction: iceAnimation, started: Date.now(), duration: 1000, delay: 2000});
+  animateIt({elFunction: animateCalfImg, started: Date.now(), duration: 600});
+
   setTimeout(() => {
-    animateIt({elFunction: iceAnimation2, started: Date.now(), duration: 1000, delay: 2000});
-  }, 1000);
-  animateCalfImg();
-  setTimeout(animateSnow1Img, 1000);
+    animateIt({elFunction: animateSnowImg1, started: Date.now(), duration: 500});
+    animateIt({elFunction: animateSnowImg2, started: Date.now(), duration: 500});
+  }, 600);
+
 
 }
