3302d479b60b3c532dc77dde4ca4580ff9f54d7e
andikk <andikk@mail.ru>
2021-06-25 17:41:54 +0300

    просто последовательное появление всего. не понятно: 1) как сделать правильное наложение (z-index); 2) как применять трансформации; 3) как делать бесконечные анимации; 4) под вопросом несколько анимаций.

--- source/js/modules/calf.js
+++ source/js/modules/calf.js
@@ -7,10 +7,27 @@ export default function animateCalf() {
     // сохраняем состояние контекста
     ctx.save();
     // рисуем сцену
+
+    // самолёт
+    drawObject({el: backImgDom, dx: back.dx, dy: back.dy, dw: back.dw, dh: back.dh, isVisible: back.isVisible});
+    drawObject({el: planeImgDom, dx: plane.dx, dy: plane.dy, dw: plane.dw, dh: plane.dh, isVisible: plane.isVisible});
+
+    // дерево
+    drawObject({el: treeImgDom, dx: tree.dx, dy: tree.dy, dw: tree.dw, dh: tree.dh, isVisible: tree.isVisible});
+
+    // дерево2
+    drawObject({el: treeImgDom2, dx: tree2.dx, dy: tree2.dy, dw: tree2.dw, dh: tree2.dh, isVisible: tree2.isVisible});
+
+    // льдина
     drawObject({el: iceImgDom, dx: ice.dx, dy: ice.dy, dw: ice.dw, dh: ice.dh, isVisible: ice.isVisible});
+    // морж
     drawObject({el: calfImgDom, dx: calf.dx, dy: calf.dy, dw: calf.dw, dh: calf.dh, isVisible: calf.isVisible});
+
+    // снег
     drawObject({el: snowImgDom1, dx: snow1.dx, dy: snow1.dy, dw: snow1.dw, dh: snow1.dh, isVisible: snow1.isVisible});
     drawObject({el: snowImgDom2, dx: snow2.dx, dy: snow2.dy, dw: snow2.dw, dh: snow2.dh, isVisible: snow2.isVisible});
+
+
     // восстанавливаем состояние контекста
     ctx.restore();
     // запускаем на отрисовку следующий кадр
@@ -109,15 +126,105 @@ export default function animateCalf() {
   };
 
 
-  tick();
-  animateIt({elFunction: animateIceImg, started: Date.now(), duration: 600});
+  // самолёт
+  const plane = {
+    dx: 130,
+    dy: 150,
+    dw: 250,
+    dh: 250,
+    src: `/img/module-4/win-primary-images/airplane.png`,
+    isVisible: 0
+  };
 
+  const planeImgDom = createImgElement(plane);
+  const animatePlane = () => {
+    plane.isVisible = 1;
+    plane.dx = (plane.dx + 10);
+    drawObject({el: planeImgDom, dx: plane.dx, dy: plane.dy, dw: plane.dw, dh: plane.dh, isVisible: plane.isVisible});
+  };
+
+  // след
+  const back = {
+    dx: 180,
+    dy: 250,
+    dw: 570,
+    dh: 320,
+    src: `/img/module-4/win-primary-images/back.png`,
+    isVisible: 0
+  };
+
+  const backImgDom = createImgElement(back);
+  const animateBack = () => {
+    back.isVisible = 1;
+    drawObject({el: backImgDom, dx: back.dx, dy: back.dy, dw: back.dw, dh: back.dh, isVisible: back.isVisible});
+  };
+
+  // дерево
+  const tree = {
+    dx: 480,
+    dy: 320,
+    dw: 35,
+    dh: 150,
+    src: `/img/module-4/win-primary-images/tree2.png`,
+    isVisible: 0
+  };
+
+  const treeImgDom = createImgElement(tree);
+  const animateTree = () => {
+    tree.isVisible = 1;
+    drawObject({el: treeImgDom, dx: tree.dx, dy: tree.dy, dw: tree.dw, dh: tree.dh, isVisible: tree.isVisible});
+  };
+
+  // дерево2
+  const tree2 = {
+    dx: 530,
+    dy: 370,
+    dw: 35,
+    dh: 100,
+    src: `/img/module-4/win-primary-images/tree.png`,
+    isVisible: 0
+  };
+
+  const treeImgDom2 = createImgElement(tree2);
+  const animateTree2 = () => {
+    tree2.isVisible = 1;
+    drawObject({el: treeImgDom2, dx: tree2.dx, dy: tree2.dy, dw: tree2.dw, dh: tree2.dh, isVisible: tree2.isVisible});
+  };
+
+
+  tick();
+
+  // анимация моржа и льдины
+  animateIt({elFunction: animateIceImg, started: Date.now(), duration: 600});
   animateIt({elFunction: animateCalfImg, started: Date.now(), duration: 600});
 
+  // след
+  setTimeout(() => {
+    animateIt({elFunction: animateBack, started: Date.now(), duration: 600});
+  }, 600);
+
+  // самолёт
+  setTimeout(() => {
+    animateIt({elFunction: animatePlane, started: Date.now(), duration: 600});
+  }, 600);
+
+
+  // дерево
+  setTimeout(() => {
+    animateIt({elFunction: animateTree, started: Date.now(), duration: 600});
+  }, 600);
+
+  // дерево2
+  setTimeout(() => {
+    animateIt({elFunction: animateTree2, started: Date.now(), duration: 600});
+  }, 600);
+
+
+  // снег
   setTimeout(() => {
     animateIt({elFunction: animateSnowImg1, started: Date.now(), duration: 500});
     animateIt({elFunction: animateSnowImg2, started: Date.now(), duration: 500});
-  }, 600);
+  }, 1200);
 
 
 }
